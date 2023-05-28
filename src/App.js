c840e2472da8404594418ffa9066955e50fc6fd9
VIGNESH NAGAVEL <72061699+vigneshnitthish@users.noreply.github.com>
2023-05-28 09:08:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { auth } from './firebase';
+import Home from "./Home"
+import Login from './Login';
+import Register from './Register';
+import AdminLogin from "./Adminlogin"
+import Admin from "./Admin"
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path = "/home" element = {<Home/>}/>
+        <Route path = "/adminlogin" element = {<AdminLogin/>}/>
+        <Route path = "/admin" element = {<Admin/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
